5a7ea101b15a1c6eed4828d90651874226222bf0
FedorovMisha <m.fedorov20021@gmail.com>
2021-05-23 03:39:50 +0300

    Menu updates

---
+++ Assets/Scripts/EdibleInedible/MoveDown.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace EdibleInedible
+{
+    public class MoveDown : MonoBehaviour
+    {
+        private void FixedUpdate()
+        {
+            gameObject.transform.position = new Vector3(gameObject.transform.position.x,
+                gameObject.transform.position.y - 0.1f, gameObject.transform.position.z);
+            if (gameObject.transform.position.y < -24f)
+            {
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
